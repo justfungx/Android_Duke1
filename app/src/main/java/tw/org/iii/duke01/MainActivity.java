6ccaaff942a8b5bf5725fbb7268e32985b1d629e
justfungx <justfungx@hotmail.com>
2016-09-09 13:06:54 +0800

    v4 +Lottery

--- app/src/main/java/tw/org/iii/duke01/MainActivity.java
+++ app/src/main/java/tw/org/iii/duke01/MainActivity.java
@@ -1,16 +1,31 @@
 package tw.org.iii.duke01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button click ;
+    private TextView mesg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("HELLO","DK");
-        Log.i("HEY","DK");
+        click = (Button)findViewById(R.id.bntClick);
+        mesg = (TextView)findViewById(R.id.mak);
+        click.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mesg.setText("Lottery:" + (int)(Math.random()*49+1));
+            }
+        });
+
+        Log.i("HELLO","D.K");
+        Log.i("DK","D&K");
     }
+
 }
